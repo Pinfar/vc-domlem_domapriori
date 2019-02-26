1c250eff419b508f7f6873c63eccdce48f1e5b69
Marcin Wilczyński <wilczynski.pgs@gmail.com>
2019-02-26 17:35:43 +0100

    Added working dependencies

--- VC.DomLEM.DomAPRIORI/VC.DomLEM.DomAPRIORI/Class1.cs
+++ VC.DomLEM.DomAPRIORI/VC.DomLEM.DomAPRIORI/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace VC.DomLEM.DomAPRIORI
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void TestFramework()
+        {
+            2.ShouldBe(2);
+        }
     }
 }
